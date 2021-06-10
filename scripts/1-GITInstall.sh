90ebd71a3fc2c8ce213bcb9e2c4675f6861cf4cf
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-06-10 15:37:39 +0100

    Create 1-GITInstall.sh

---
+++ scripts/1-GITInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# by: JOR
+# Date: 10JAN20
+# Function: Configure GIT
+# Script: GITInstall.sh
+
+clear
+echo "*******************************************************"
+echo "Welcome" $USER
+echo "This script will create keys for new Ansible controller"
+echo "Edit the script as needs"
+echo "*******************************************************"
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/git.rsa -C admin@iotech.ie
+
+# eval `ssh-agent`
+ssh-add ~/.ssh/git.rsa
+
+git clone git@github.com:IOTECH-Donegal/UB1804.git
+git status
+git config --global user.name "IOTECH-Donegal"
+git config --global user.email "ansible1@iotech.ie"
+git config --list
+echo '*************************************************'
+echo 'Hey' $USER ', all done!'
