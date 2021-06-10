09bc6dad679a58c7949e5a15b958336bab9f1f1c
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-06-10 15:40:41 +0100

    Update 1-GITInstall.sh

--- scripts/1-GITInstall.sh
+++ scripts/1-GITInstall.sh
@@ -7,16 +7,18 @@
 clear
 echo "*******************************************************"
 echo "Welcome" $USER
-echo "This script will create keys for new Ansible controller"
+echo "This script will create keys for new RPi"
 echo "Edit the script as needs"
 echo "*******************************************************"
 
+echo "Creating Unique Key"
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/git.rsa -C admin@iotech.ie
+echo "Now copy this up to the GIT account"
 
 # eval `ssh-agent`
 ssh-add ~/.ssh/git.rsa
 
-git clone git@github.com:IOTECH-Donegal/UB1804.git
+git clone git@github.com:IOTECH-Donegal/Raspbian.git
 git status
 git config --global user.name "IOTECH-Donegal"
 git config --global user.email "ansible1@iotech.ie"
